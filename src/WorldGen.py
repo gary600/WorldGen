cbd6df327ba827e71c2705460976c3ecfec47c64
Gary B <gary600playsmc@gmail.com>
2017-06-09 15:43:32 -0600

    Worked on processor class

--- src/WorldGen.py
+++ src/WorldGen.py
@@ -9,7 +9,7 @@ import threading
 
 #endregion
 
-class RegionProcessor:
+class RegionProcessor(threading.Thread):
     def __init__(self, startX, startY, endX, endY, totalWidth, totalHeight, tnoise, mnoise):
         self.startX = startX
         self.startY = startY
@@ -24,14 +24,15 @@ class RegionProcessor:
         self.terrain = Image.new("L", (self.width, self.height))
         self.moisture = Image.new("L", (self.width, self.height))
         self.world = Image.new("RGB", (self.width, self.height))
+        super.__init__(self)
 
-    def __call__(self):
+    def run(self):
         self.genNoise()
         self.addSlope()
         self.addColor()
 
     def genNoise(self):
-        tdraw = ImageDraw.Draw(self.terain)
+        tdraw = ImageDraw.Draw(self.terrain)
         mdraw = ImageDraw.Draw(self.moisture)
         for x in range(self.width):
             for y in range(self.height):
@@ -41,8 +42,49 @@ class RegionProcessor:
 
     def addSlope(self):
         maxDist = math.sqrt((self.totalWidth / 2)**2 + (self.totalHeight / 2)**2)
-        tdraw = ImageDraw.Draw(self.terain)
+        tdraw = ImageDraw.Draw(self.terrain)
         for x in range(self.width):
             for y in range(self.height):
-                pass
-                # tdraw.point((x, y), )
+                dist = math.sqrt(((self.totalWidth - (x + self.startX)) / 2)**2 + (self.totalHeight - (y + self.startY) / 2)**2)
+                tdraw.point((x, y), self.terrain.getpixel((x, y)) - ((dist / maxDist) * 127))
+        del tdraw
+
+    def addColor(self):
+        wdraw = ImageDraw.Draw(self.world)
+        for x in range(self.width):
+            for y in range(self.height):
+                e = self.terrain.getpixel((x, y))
+                m = self.moisture.getpixel((x, y))
+                # Hot pink to show gaps
+                color = (255,182, 193)
+                # Ocean
+                if e < 47:
+                    color = (0, 119, 190)
+                # Beach
+                elif e < 57:
+                    color = (194, 178, 128)
+                # Bare rock
+                elif e > 180:
+                    color = (128, 132, 135)
+                # Tundra
+                elif e > 190:
+                    color = (219, 255, 255)
+                else:
+                    # Desert
+                    if m < 100:
+                        color = (194, 178, 128)
+                    # Grassland
+                    elif m < 130:
+                        color = (77, 189, 51)
+                    # Forest
+                    elif m < 160:
+                        color = (34, 139, 34)
+                    # Rainforest
+                    else:
+                        color = (69, 139, 0)
+                wdraw.point((x, y), color)
+        del wdraw
+
+    def getResult(self):
+        return self.world
+
