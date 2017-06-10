fd095f1be32c030d89354757c94054294ad9a2ca
Gary B <gary600playsmc@gmail.com>
2017-06-10 09:57:03 -0600

    Added all of the logic that controls the threads, and finished the threads themselves.

--- src/WorldGen.py
+++ src/WorldGen.py
@@ -1,16 +1,51 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw,ImageFont
 from opensimplex import OpenSimplex
 import math
 import random
 import argparse
 import threading
+import time
 
-#region Argument Parsing
+startTime = time.time()
 
-#endregion
+# region Argument Parsing
+parser = argparse.ArgumentParser(prog="WorldGen", description="Generates a world.")
+
+parser.add_argument(
+    "filename",
+    type=str,
+    help="File to store the result in"
+)
+parser.add_argument(
+    "--no-label",
+    action="store_const",
+    type=bool,
+    default=False,
+    const=True,
+    dest="label",
+    nargs=0,
+    help="Disables the label with the seed"
+)
+parser.add_argument(
+    "-s", "--seed",
+    type=int,
+    default=random.randint(1, 10000),
+    dest="seed",
+    help="Sets the seed of the world."
+)
+parser.add_argument(
+    "-t", "--threads",
+    type=int,
+    default=4,
+    dest="threads",
+    help="Sets how many threads will be utilized by the generator"
+)
+args = parser.parse_args()
+
+# endregion
 
 class RegionProcessor(threading.Thread):
-    def __init__(self, startX, startY, endX, endY, totalWidth, totalHeight, tnoise, mnoise):
+    def __init__(self, startX, startY, endX, endY, totalWidth, totalHeight, tnoise, mnoise, output):
         self.startX = startX
         self.startY = startY
         self.endX = endX
@@ -24,12 +59,14 @@ class RegionProcessor(threading.Thread):
         self.terrain = Image.new("L", (self.width, self.height))
         self.moisture = Image.new("L", (self.width, self.height))
         self.world = Image.new("RGB", (self.width, self.height))
+        self.out = output
         super.__init__(self)
 
     def run(self):
         self.genNoise()
         self.addSlope()
         self.addColor()
+        self.out[(self.startX, self.startY)] = self.world
 
     def genNoise(self):
         tdraw = ImageDraw.Draw(self.terrain)
@@ -85,6 +122,58 @@ class RegionProcessor(threading.Thread):
                 wdraw.point((x, y), color)
         del wdraw
 
-    def getResult(self):
-        return self.world
 
+if __name__ == "__main__":
+    # Calculate the dimensions of the chunk for each thread
+    x = args.threads
+    n = math.ceil(math.sqrt(x))
+    while x % n != 0:
+        n += 1
+    threadDimX = n
+    threadDimY = x / n
+
+    if 480 % threadDimX != 0 or 480 % threadDimY != 0:
+        print("Error: Thread count not divisible into resolution (480x480)")
+        raise SystemExit
+    threadSize = (480 / threadDimX, 480 / threadDimY)
+
+    # Create generator threads
+    tnoise = OpenSimplex(seed=args.seed)
+    random.seed(args.seed)
+    mnoise = OpenSimplex(seed=random.randint(1, 10000))
+    pool = []
+    outputs = {}
+    for x in range(threadDimX):
+        for y in range(threadDimY):
+            pool.append(RegionProcessor(
+                threadSize[0]*x, threadSize[1]*y,
+                threadSize[0]*(x+1)-1, threadSize[1]*(y+1)-1,
+                480, 480,
+                tnoise, mnoise,
+                outputs
+            ))
+
+    # Start threads
+    for thread in pool:
+        thread.start()
+
+    # Wait for threads to finish
+    while [x.is_alive() for x in pool].count(True) > 0:
+        print("{} thread(s) are done".format([x.is_alive() for x in pool].count(False)), end="\r")
+    print()
+
+    # Stitch together final image
+    finalImg = Image.new("RGB", (480, 480))
+    for pos, img in outputs.items():
+        finalImg.paste(img, pos)
+
+    # Add label
+    if args.label:
+        draw = ImageDraw.Draw(finalImg)
+        draw.text((10, 10), "Seed: {}".format(args.seed), font=ImageFont.load("arial"))
+
+    # Save image
+    with open(args.filename, "wb") as f:
+        finalImg.save(f)
+
+    print("Done! Took {} seconds.".format(time.time() - startTime))
